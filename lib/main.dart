1b2d66a2690bb3c1dd980490bba3fe14c23f0a49
Reema Asker <reema1822000@gmail.com>
2023-09-10 20:59:07 +0300

    update post request,signup and put the user name in its profile in the home

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-// import 'package:gsg_final_project_rgs/view_features/auth/widgets/testUi.dart';
-// import 'package:gsg_final_project_rgs/view_features/home/categories/providers/Category_provider.dart';
 import 'package:gsg_final_project_rgs/view_features/splash/widgets/splash_screan.dart';
-// import 'package:provider/provider.dart';
 
 void main() async {
   runApp(const MyApp());
+  // TagRepo tagRepo = TagRepo();
+  // tagRepo.fetchAllTagsWithoutEmail();
+  // tagRepo.fetchTagsWithEmail(tags: [2, 3]);
+  // tagRepo.createTag("salah");
+  // tagRepo.fetchTagsOfMail(mailId: 2);
+  // StatusRepo statusRepo = StatusRepo();
+  // statusRepo.fetchStatuses(withMail: false);
+  // statusRepo.fetchOneStatusWithMails(1);
+  // statusRepo.statusMailsCount(1);
+  // CategoryRepo categoryRepo = CategoryRepo();
+  // categoryRepo.fetchCategory(2);
+  // categoryRepo.fetchCategoriesList();
+  // categoryRepo.createCategory("name");
 }
 
 class MyApp extends StatelessWidget {
@@ -13,26 +23,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-        // MultiProvider(
-        // providers: [
-        //   ChangeNotifierProvider<AuthProvider>(
-        //     create: (_) => AuthProvider(),
-        //   ),
-        //   ChangeNotifierProvider<CategoryProvider>(
-        //     create: (_) => CategoryProvider(),
-        //   ),
-        // ],
-        // child: MaterialApp(
-        MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'home',
-      home: SplashScreen(),
-      // routes: {
-      //   SplashScreen.id: (context) => SplashScreen(),
-      //   Hello.id: (context) => Hello(),
-      // }
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, title: 'home', home: SplashScreen());
+    // return MultiProvider(
+    //   providers: [
+    //     // ChangeNotifierProvider<CategoryProvider>(
+    //     //   create: (_) => CategoryProvider(),
+    //     // ),
+    //   ],
+    //   child: const MaterialApp(
+    //       debugShowCheckedModeBanner: false,
+    //       title: 'home',
+    //       home: SplashScreen()),
     // );
   }
 }
